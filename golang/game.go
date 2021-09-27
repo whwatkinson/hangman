544f04dcac04aec9d5bef4f4cfc1405d825e5f51
harrywatkinson <harry@revmo.app>
2021-09-27 10:04:42 +0100

    wip

---
+++ golang/game.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
+const filePathWords string = "../words.txt"
+const lives int = 10
+
+func random_word_generator() string {
+	var wordsString string = "apple pop snap cat rock"
+	var words = strings.Fields(wordsString)
+	var wordsListLength int = len(words)
+	var randomNumber = rand.Intn(wordsListLength)
+
+	fmt.Printf("%v %T", words, words)
+
+	var word string = words[randomNumber]
+	return word
+}
+
+var word = random_word_generator()
+
+func main() {
+	// fmt.Printf("%v %T", word, word)
+}
