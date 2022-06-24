f1aa558c4e2bb87d6b99c3791970270e4cee24d8
Harry Watkinson <harry@revmo.app>
2022-06-24 16:40:35 +0100

    picking the word

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,25 +1,28 @@
-// in browser
 import {readFileSync} from 'fs';
-
-// out of browser
-// const {readFileSync, promises: fsPromises} = require('fs');
 
 
 const FILE_PATH_WORDS: String = "words.txt"
-// let LIVES: Number = 10
+let LIVES: Number = 10
 
 
-function syncReadFile(filename: string)  {
+function randomWordGenerator(filename: string)  {
     const contents = readFileSync(filename, 'utf-8');
   
     const words = contents.split(/\r?\n/);
 
     var word = words[Math.floor(Math.random()*words.length)];
-
   
     console.log(word);
   
     return word;
   }
 
-  syncReadFile("words.txt")
+
+function hangman(){
+  let word: string = randomWordGenerator("words.txt")
+}
+
+
+if (require.main === module) {
+  hangman();
+}
