8f1d9e90425ad36d1b1f91f56f8dd4cb630b7590
Harry Watkinson <harry@revmo.app>
2022-06-24 17:05:09 +0100

    vars set up

--- typescript/notes.ts
+++
@@ -1,5 +0,0 @@
-// in browser
-import {readFileSync} from 'fs';
-
-// out of browser
-// const {readFileSync, promises: fsPromises} = require('fs');
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,25 +1,83 @@
-import {readFileSync} from 'fs';
+import { readFileSync } from 'fs';
 
 
-const FILE_PATH_WORDS: String = "words.txt"
+const FILE_PATH_WORDS: string = "words.txt"
 let LIVES: Number = 10
 
 
-function randomWordGenerator(filename: string)  {
-    const contents = readFileSync(filename, 'utf-8');
-  
-    const words = contents.split(/\r?\n/);
+function randomWordGenerator(filename: string): string {
+  const contents = readFileSync(filename, 'utf-8');
 
-    var word = words[Math.floor(Math.random()*words.length)];
-  
-    console.log(word);
-  
-    return word;
+  const words: string[] = contents.split(/\r?\n/);
+
+  var word: string = words[Math.floor(Math.random() * words.length)];
+
+  return word;
+}
+
+
+function hangman() {
+  let word: string = randomWordGenerator(FILE_PATH_WORDS)
+  let wordChars: string[] = word.split("")
+  let lettersRemaining: string[] = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "-",
+  ]
+  var lives: Number = LIVES
+  var roundNumber: Number = 0
+  var wordBlank = []
+
+  // in lieu of a list comprehension 
+  for (let i =0; i < word.length; i++){
+    wordBlank.push('_')
   }
 
+  var rightGuess: string[] = []
+  var wrongGuess: string[] = []
 
-function hangman(){
-  let word: string = randomWordGenerator("words.txt")
+  // Start and welcome
+  console.log("******************")
+  console.log("Welcome to Hangman")
+  console.log("******************\n")
+  
+
+
+
+
+  console.log(word)
+  console.log(wordChars)
+  console.log(lettersRemaining)
+  console.log(lives)
+  console.log(roundNumber)
+  console.log(wordBlank)
+  console.log(rightGuess)
+  console.log(wrongGuess)
+
 }
 
 
